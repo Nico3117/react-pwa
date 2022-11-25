5f1c08def1c9856fdb4d930fb868512d166a1b45
Nico3117 <nicobaca17@gmail.com>
2022-11-25 11:59:27 +0100

    pwa

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Test = ({title}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+let Boolean = true;
+
 function App() {
+  
+  const Kaiser = ["1", "2", "3"]
+
+  const Chewye = Boolean ? <span>True</span> : <span>False</span>
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test title="Nico"></Test>
+        {
+          Boolean && <span>Test if</span>
+        }
+        {
+          Kaiser.map((element, i) => {
+            return <span key={i}>{element}</span>
+          })
+        }
+        {Chewye}
       </header>
     </div>
   );
 }
 
-export default App;
+// export default App;
+export {Test, App}
